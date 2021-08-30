946f1f1c024eaaf014d197a28d64d1b35bd3b44e
Konrad <konrad.gmur@gmail.com>
2021-08-30 20:41:11 +0200

    Downloading data using the axios library

--- pages/index.vue
+++ pages/index.vue
@@ -5,5 +5,21 @@
 </template>
 
 <script>
-export default {}
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      movies: [],
+    }
+  },
+  methods: {
+    async getMovies() {
+      const data = axios.get(
+        `https://api.themoviedb.org/3/search/movie?api_key=b0e7142d1912dc6e432dcfdb40d564fd&language=en-US&page=1&query=${this.searchInput}`
+      )
+      const result = await data
+      console.log(result)
+    },
+  },
+}
 </script>
