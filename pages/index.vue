b68ae6afaa42454c6b8c0ed16f5407b3ece5953a
Konrad <konrad.gmur@gmail.com>
2021-08-31 19:59:04 +0200

    Improving the error when downloading data

--- pages/index.vue
+++ pages/index.vue
@@ -18,10 +18,13 @@ export default {
   methods: {
     async getMovies() {
       const data = axios.get(
-        `https://api.themoviedb.org/3/search/movie?api_key=b0e7142d1912dc6e432dcfdb40d564fd&language=en-US&page=1&query=${this.searchInput}`
+        `https://api.themoviedb.org/3/movie/now_playing?api_key=b0e7142d1912dc6e432dcfdb40d564fd&language=en-US&page=1`
       )
       const result = await data
-      console.log(result.data)
+      result.data.results.forEach((movie) => {
+        this.movies.push(movie)
+      })
+      console.log(this.movies)
     },
   },
 }
