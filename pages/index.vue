3bc0da7dc8856e60ad49549f647f05f9d932aacb
Konrad <konrad.gmur@gmail.com>
2021-08-31 20:33:37 +0200

    Displaying the entire component + Bug repair

--- pages/index.vue
+++ pages/index.vue
@@ -8,11 +8,32 @@
         <div class="movie" v-for="(movie, index) in movies" :key="index">
           <div class="movie-img">
             <img
-              :src="`https://image.tmdb.org/t/p/${movie.poster_path}`"
+              :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`"
               alt=""
             />
             <p class="review">{{ movie.vote_average }}</p>
             <p class="overview">{{ movie.overview }}</p>
+          </div>
+          <div class="info">
+            <p class="title">
+              {{ movie.title.slice(0, 25) }}
+              <span v-if="movie.title.length > 25">...</span>
+            </p>
+            <p class="release">
+              Released:
+              {{
+                new Date(movie.release_date).toLocaleString('en-us', {
+                  month: 'long',
+                  day: 'numeric',
+                  year: 'numeric',
+                })
+              }}
+            </p>
+            <NuxtLink
+              class="button button-light"
+              :to="{ name: 'movies-movieid', params: { movieid: movie.id } }"
+              >Get More Info</NuxtLink
+            >
           </div>
         </div>
       </div>
