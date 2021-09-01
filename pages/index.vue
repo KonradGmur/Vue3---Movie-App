f896c5d6026f03b6809dd6c62073348f5a2cb9ae
Konrad <konrad.gmur@gmail.com>
2021-09-01 19:28:00 +0200

    Improving the Input operation when searching movie

--- pages/index.vue
+++ pages/index.vue
@@ -11,12 +11,53 @@
         placeholder="Search"
         v-model.lazy="searchInput"
       />
-      <button v-show="searchInput !== ''" class="button">Clear Search</button>
+      <button @click="clearSearch" v-show="searchInput !== ''" class="button">
+        Clear Search
+      </button>
     </div>
 
     <!-- Movies -->
     <div class="container movies">
-      <div id="movie-grid" class="movies-grid">
+      <!-- Searched Movies-->
+      <div v-if="searchInput !== ''" id="movie-grid" class="movies-grid">
+        <div
+          class="movie"
+          v-for="(movie, index) in searchedMovies"
+          :key="index"
+        >
+          <div class="movie-img">
+            <img
+              :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`"
+              alt=""
+            />
+            <p class="review">{{ movie.vote_average }}</p>
+            <p class="overview">{{ movie.overview }}</p>
+          </div>
+          <div class="info">
+            <p class="title">
+              {{ movie.title.slice(0, 25) }}
+              <span v-if="movie.title.length > 25">...</span>
+            </p>
+            <p class="release">
+              Released:
+              {{
+                new Date(movie.release_date).toLocaleString('en-us', {
+                  month: 'long',
+                  day: 'numeric',
+                  year: 'numeric',
+                })
+              }}
+            </p>
+            <NuxtLink
+              class="button button-light"
+              :to="{ name: 'movies-movieid', params: { movieid: movie.id } }"
+              >Get More Info</NuxtLink
+            >
+          </div>
+        </div>
+      </div>
+      <!-- Now Streaming-->
+      <div v-else id="movie-grid" class="movies-grid">
         <div class="movie" v-for="(movie, index) in movies" :key="index">
           <div class="movie-img">
             <img
@@ -80,7 +121,6 @@ export default {
       result.data.results.forEach((movie) => {
         this.movies.push(movie)
       })
-      console.log(this.movies)
     },
     async searchMovies() {
       const data = axios.get(
@@ -90,7 +130,10 @@ export default {
       result.data.results.forEach((movie) => {
         this.searchedMovies.push(movie)
       })
-      console.log(this.searchedMovies)
+    },
+    clearSearch() {
+      this.searchInput = ''
+      this.searchedMovies = []
     },
   },
 }
