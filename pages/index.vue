88e4083c3b0e6acbc997cd163e92a06b04077656
Konrad <konrad.gmur@gmail.com>
2021-09-04 12:54:51 +0200

    Adding loading.vue component before loading data

--- pages/index.vue
+++ pages/index.vue
@@ -16,8 +16,11 @@
       </button>
     </div>
 
+    <!-- Loading -->
+    <Loading v-if="$fetchState.pending" />
+
     <!-- Movies -->
-    <div class="container movies">
+    <div v-else class="container movies">
       <!-- Searched Movies-->
       <div v-if="searchInput !== ''" id="movie-grid" class="movies-grid">
         <div
@@ -112,6 +115,7 @@ export default {
 
     await this.searchMovies()
   },
+  fetchDelay: 1000,
   methods: {
     async getMovies() {
       const data = axios.get(
