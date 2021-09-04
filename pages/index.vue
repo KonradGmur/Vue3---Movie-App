f8db048a0e3347ef1a0fe8c087c30aa880453b8a
Konrad <konrad.gmur@gmail.com>
2021-09-04 13:49:21 +0200

    Adding the title and META tag to the home page

--- pages/index.vue
+++ pages/index.vue
@@ -100,6 +100,23 @@
 <script>
 import axios from 'axios'
 export default {
+  head() {
+    return {
+      title: 'Movie App',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Get all the latest streaming movies in theaters & online',
+        },
+        {
+          hid: 'keywords',
+          name: 'keywords',
+          content: 'movies, stream, streaming',
+        },
+      ],
+    }
+  },
   data() {
     return {
       movies: [],
